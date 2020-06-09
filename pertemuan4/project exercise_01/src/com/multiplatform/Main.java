7ebd069400450a8323013e73c70497b60ee092a5
brigitasyaloom <s21710489@student.unklab.ac.id>
2020-06-09 16:18:42 +0800

    pertemuan4 dan excercise 1

---
+++ pertemuan4/project exercise_01/src/com/multiplatform/Main.java
@@ -0,0 +1,14 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte bytevalue = 30;
+        short shortvalue = 500;
+        int intvalue = 1000;
+
+        long longValue = (50000 + 10L ) * (bytevalue + shortvalue + intvalue);
+        System.out.println("longValue  = " + longValue);
+
+    }
+}
